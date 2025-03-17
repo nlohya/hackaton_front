edc7cd747a45ebf64b740f0846d360f7bb0dc1bc
Nicolas Lohya <nicolas.lohya.pro@gmail.com>
2025-03-17 16:06:53 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <Clock />
+</template>
+<script setup lang="ts">
+import Clock from "../src/components/Clock.vue";
+</script>
---
+++ src/components/Clock.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 class="text-3xl">{{ time }}</h1>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const time = ref<string>("");
+
+function updateTime() {
+  var cd = new Date();
+  time.value =
+    zeroPadding(cd.getHours(), 2) + ":" + zeroPadding(cd.getMinutes(), 2);
+}
+
+function zeroPadding(num, digit) {
+  var zero = "";
+  for (var i = 0; i < digit; i++) {
+    zero += "0";
+  }
+  return (zero + num).slice(-digit);
+}
+
+onMounted(() => {
+  setInterval(updateTime, 1000);
+  updateTime();
+});
+</script>
