dff88be35a871183c1f580797a2658eb24fbe7c2
Nicolas Lohya <nicolas.lohya.pro@gmail.com>
2025-03-18 15:14:47 +0100

    tfutf

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@
       <p class="text-3xl">Musi'Quiz</p>
     </div>
 
-    <div
-      class="p-4 h-full flex flex-col items-left justify-center gap-4 motion-preset-blink"
-    >
-      <div class="p-4 rounded-md border border-white w-fit space-y-4">
+    <div class="p-4 h-full flex flex-col items-left justify-center gap-4">
+      <div
+        class="session-box p-4 rounded-md border border-white w-fit space-y-4 animate-glow"
+      >
         <h2 class="text-7xl">Session en cours : {{ events[0]?.name }}</h2>
         <p class="text-xl">
           Début : {{ events[0]?.start }} - Fin : {{ events[0]?.stop }}
@@ -30,6 +30,48 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+@keyframes glow {
+  0% {
+    box-shadow: 0 0 10px rgba(255, 255, 255, 0.3);
+  }
+  50% {
+    box-shadow: 0 0 20px rgba(255, 255, 255, 0.7);
+  }
+  100% {
+    box-shadow: 0 0 10px rgba(255, 255, 255, 0.3);
+  }
+}
+
+@keyframes border-color {
+  0% {
+    box-shadow: 0 0 10px red;
+    border-color: red;
+  }
+  25% {
+    box-shadow: 0 0 10px orange;
+    border-color: orange;
+  }
+  50% {
+    box-shadow: 0 0 10px yellow;
+    border-color: yellow;
+  }
+  75% {
+    box-shadow: 0 0 10px orange;
+    border-color: orange;
+  }
+  100% {
+    box-shadow: 0 0 10px red;
+    border-color: red;
+  }
+}
+
+.session-box {
+  animation: border-color 2s infinite;
+}
+</style>
+
 <script setup lang="ts">
 import Clock from "../src/components/Clock.vue";
 import Line from "../src/assets/Line.png";
@@ -48,7 +90,7 @@ function refreshData() {
 }
 
 onMounted(() => {
-  setInterval(refreshData, 30000);
+  setInterval(refreshData, 1000);
   refreshData();
 });
 </script>
